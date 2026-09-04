0326be6045eb971d197ede19540c37a80d428657
SAILE-hub <eliasgamingoficial@gmail.com>
2026-09-04 17:43:00 -0400

    carpetas

---
+++ WEBCMSSanValero/config/bd.php
@@ -0,0 +1,18 @@
+<?php
+
+ return array (
+
+    'driver' => 'mysql',
+
+    'host' => 'localhost',
+
+    'user' => 'root',
+
+    'database' => 'my_database',
+
+    'password' => 'secret',
+    
+    'charset' => 'utf8mb4',
+
+    );
+?>
